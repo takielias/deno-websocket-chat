737dfe5f19e4bbd875df6cf83db4a9db0ade9719
takielias <taki.elias@gmail.com>
2020-06-10 20:30:35 +0600

    Middleware updated with ctx.upgrade() to initiate the WS

--- middleware.ts
+++ middleware.ts
@@ -1,64 +1,17 @@
 import {
-  acceptWebSocket,
-  isWebSocketCloseEvent,
-  isWebSocketPingEvent,
+  WebSocket,
 } from "./deps.ts";
 
+export const socks: WebSocket[] = [];
+
+export const wsPathname = "/ws";
+
 export const middleware = async function (ctx: any, next: any) {
-  if (ctx.request.url.pathname !== "/") {
+  if (ctx.request.url.pathname !== wsPathname) {
     return await next();
   }
-
-  if (ctx.request.url.pathname !== "/ws") {
-    return await next();
-  }
-
-  if (ctx.request.headers.get("upgrade") !== "websocket") {
-    ctx.response.status = 200;
-    return;
-  }
-
-  const { conn, r: bufReader, w: bufWriter, headers } =
-    ctx.request.serverRequest;
-
-  try {
-    const sock = await acceptWebSocket({
-      conn,
-      bufReader,
-      bufWriter,
-      headers,
-    });
-    try {
-      for await (const ev of sock) {
-        if (typeof ev === "string") {
-          // text message
-          console.log("ws:Text", ev);
-
-          // await sock.send(ev);
-
-          sock.send(ev);
-        } else if (ev instanceof Uint8Array) {
-          // binary message
-          console.log("ws:Binary", ev);
-        } else if (isWebSocketPingEvent(ev)) {
-          const [, body] = ev;
-          // ping
-          console.log("ws:Ping", body);
-        } else if (isWebSocketCloseEvent(ev)) {
-          // close
-          const { code, reason } = ev;
-          console.log("ws:Close", code, reason);
-        }
-      }
-    } catch (err) {
-      console.error(`Dev Server failed to receive frame: ${err}`);
-
-      if (!sock.isClosed) {
-        await sock.close(1000).catch(console.error);
-      }
-    }
-  } catch (err) {
-    console.error(`Dev Server failed to accept websocket: ${err}`);
-    ctx.response.status = 400;
-  }
+  const sock = await ctx.upgrade();
+  let id = socks.push(sock) - 1;
+  for await (const ev of sock);
+  socks.splice(id, 1);
 };
